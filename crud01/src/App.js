ca8e663ea9758d7adeec9e4531bc0d168b49a8c6
Paulo henrique <paulobw012345@gmail.com>
2023-08-22 21:16:26 -0300

    reactjs google project

--- crud01/src/App.js
+++ crud01/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Email from "./components/Email";
 import Favorito from "./components/Favorito";
 
 function App() {
   return (
     <div className="Container">
       <Favorito />
+      <Email />
     </div>
   );
 }
