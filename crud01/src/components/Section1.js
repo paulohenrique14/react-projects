f562f788332bcbf3bb93a44f88854524c4e4f36c
Paulo henrique <paulobw012345@gmail.com>
2023-08-31 19:57:08 -0300

    reactjs project

--- crud01/src/components/Section1.js
+++ crud01/src/components/Section1.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from './Section1.module.css'
-const Section1 = () => {
+import { BsEarbuds } from 'react-icons/bs';
+
+const Section1 = (props) => {
   return (
     <div className={styles.container}>
         <img src="/imgs/youtube.png" alt="" />
-        <div className={styles.search}></div>
+        <div className={styles.search}>
+          <p>Lupa</p>
+          <p>pesquisa sei la o que</p>
+          <p>icone</p>
+          <p>icone</p>
+          <p><BsEarbuds/></p>
+
+        </div>
     </div>
   )
 }
