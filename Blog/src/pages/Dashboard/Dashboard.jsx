70ba3118d744dd9c375c15047955ab95a811fe0d
Paulo Henrique <paulobw012345@gmail.com>
2024-01-29 23:25:41 -0300

    fixing and adding some bugs lol

--- Blog/src/pages/Dashboard/Dashboard.jsx
+++ Blog/src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {collection, query, where } from 'firebase/firestore'
 import { db } from '../../firebase/config'
 import { useAuthValue } from '../../context/AuthContext'
@@ -8,7 +8,7 @@ import { useState } from 'react'
 const Dashboard = () => {
   const postRef = collection(db, "posts")
   const [user] = useAuthValue();
-  const {queryResult, setQueryResult} = useState([]);
+  const [queryResult, setQueryResult] = useState([]);
 
   const handleSubmit = async() => {
     const q = query(postRef, where("uid", "==", user.uid))
@@ -17,18 +17,23 @@ const Dashboard = () => {
     const data = await getDocs(q);
 
     setQueryResult(data.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+    console.log(data.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+    // console.log(queryResult.length)
   }
+
+  useEffect(() => {
+    console.log(queryResult.length)
+  }, [queryResult])
   return (
     <div>
         <h1>Gerencie aqui suas postagens</h1>
         <button onClick={handleSubmit}>teste</button>
-        {queryResult && 
-            queryResult.docs.map((doc) => {
-              // console.log(doc.id, "=>", doc.data())
-              <>
-                <h1>teste</h1>
-              </>
-            })
+        {queryResult.length > 0 && 
+          queryResult.map((doc) => {
+            <>    
+              <p key={doc.id}>{doc.image}</p>
+            </>
+          })
         }
     </div>
   )
